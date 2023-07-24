ca2096c68cf4ccfef6c6fe3ca5a379c6b254eba7
EverydayBruh <roliki2222222@gmail.com>
2023-07-24 16:35:16 +0300

    vs

--- VS_Assembler/Assembler/main.cpp
+++ VS_Assembler/Assembler/main.cpp
@@ -1,10 +1,28 @@
+// main.cpp
 #include <iostream>
 
-using namespace std;
-
-extern "C" int asmfunc();
-
-int main() {
-	cout << "result is " << asmfunc();
-	return 0;
+// Прототип функции на ассемблере с использованием extern "C"
+// Ваш C++ код
+extern "C" {
+    int* processMatrix(int* matrix, int rows, int cols);
 }
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int* result = processMatrix(&matrix[0][0], 3, 3);
+
+    std::cout << "\nResult Matrix (After multiplying by 2):" << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << result[i * 3 + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
--- VS_Assembler/Assembler/somefunc.asm
+++ VS_Assembler/Assembler/somefunc.asm
@@ -1,77 +1,37 @@
-bits 64
+.code
+processMatrix proc
 
-section .data
-;a dd 2
-;b dw 2
-;c dw 1
-;d dw 2
-;e dd 4
-a dd 2147480000
-b dw -32000
-c dw -32000
-d dw 32000
-e dd 2147480000
+	; Function prologue
+	push rbp
+	mov rbp, rsp
 
-div_by_zero_msg db 'Error: Division by zero',0
-div_by_zero_msg_len equ $-div_by_zero_msg
-msg db "%ld", 10, 0 ;форматная строка для вывода числа в десятичном формате
+	; Parameters:
+	; [rbp+16] contains the matrix pointer (int* matrix)
+	; [rbp+20] contains the number of rows (int rows)
+	; [rbp+24] contains the number of columns (int cols)
 
+	; Your matrix processing code goes here.
 
-section .text
-global _start
+	; Load the matrix pointer into rsi (second parameter)
+	mov rsi, [rbp+16]
 
-_start:
-    ;вычисление числителя
-    movsx rax, word[b] ;rax = b
-    movsx rdx, word[c] ;rdx = c
-    add rax, rdx ;rax = b + c
-    movsx rcx, dword[a];
-    imul  rcx;rax = a * (b + c)
-    movsx rbx, dword[e] ;rbx = e
-    movsx rcx, dword[a];
-    add rbx, rcx ;rbx = e + a
-    movsx rdx, word[d] ;rdx = d
-    imul rdx, rbx ;rdx = d * (e + a)
-    me:
-    sub rax, rdx ;rax = a * (b + c) - d * (e + a)
+	; Load the number of rows into rdx (third parameter)
+	mov rdx, [rbp+20]
 
-    ;вычисление знаменателя
-    movsx rdx, word[c] ;rdx = c
-    imul rdx, rdx ;rdx = c * c
-    movsx rcx, word[b] ;rcx = b
-    imul rcx, rdx ;rcx = c * c * b
-    movsx rdx, word[d] ;rdx = d
-    imul rdx, rdx ;rdx = d * d
-    sub rdx, rcx ;rdx = d * d - c * c * b
+	; Load the number of columns into rcx (fourth parameter)
+	mov rcx, [rbp+24]
 
-    ;проверка знаменателя на ноль
-    cmp rdx, 0
-    je divide_by_zero_error
+	; Loop through the matrix and multiply all the elements by 2
+	processMatrixLoop:
+		mov eax, [rsi]        ; Load the current element (4 bytes) into the lower 32 bits of rax
+		shl eax, 1           ; Multiply the current element by 2 (shift left by 1 bit)
+		mov [rsi], eax        ; Store the updated element back into the matrix
+		add rsi, 4           ; Move the matrix pointer to the next element (assuming the matrix is an int* array)
+		loop processMatrixLoop ; Loop until all elements are processed
 
-    ;вычисление результатa
-    
-    mov rbx, rdx
-    me2:
-    cqo
-    idiv rbx ;rax = (a * (b + c) - d * (e + a)) / (d * d - c * c * b)
-    me3:
-    
-    end:
-    mov rax, 1 ;устанавливаем номер системного вызова для exit
-    mov rbx, 0 ;устанавливаем код возврата 0
-    int 0x80 ;вызываем системный вызов exit
+	; Function epilogue
+	pop rbp
+	ret
 
-    
-
-divide_by_zero_error:
-    ;вывод сообщения об ошибке деления на ноль
-    mov rax, 1 ;устанавливаем номер системного вызова для write
-    mov rcx, div_by_zero_msg ;устанавливаем адрес сообщения
-    mov rdx, div_by_zero_msg_len ;устанавливаем длину сообщения
-    int 0x80 ;вызываем системный вызов write
-
-    ;выход из программы с ошибкой
-    mov rax, 1 ;устанавливаем номер системного вызова для exit
-    mov rbx, 1 ;устанавливаем код возврата 1
-    int 0x80 ;вызываем системный вызов exit
-
+processMatrix endp
+end
