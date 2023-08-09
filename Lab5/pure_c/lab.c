c0b328f4cec5fccde871b04d0a198fa0adce4d2c
EverydayBruh <roliki2222222@gmail.com>
2023-08-10 02:09:07 +0300

    Lab5 Start

--- Lab5/pure_c/lab.c
+++ Lab5/pure_c/lab.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../stb_image/stb_image.h"
@@ -8,10 +9,36 @@
 #include "../stb_image/stb_image_write.h"
 
 
+int gray(unsigned char *img, unsigned char *gray_img, size_t img_size){
+
+    for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += 3, pg += 1) {
+        *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+    }
+    return 0;
+}
+
+int sepia(unsigned char *img, unsigned char *sepia_img, size_t img_size){
+    // Sepia filter coefficients from https://stackoverflow.com/questions/1061093/how-is-a-sepia-tone-created
+    for(unsigned char *p = img, *pg = sepia_img; p != img + img_size; p += 3, pg += 3) {
+        *pg       = (uint8_t)fmin(0.393 * *p + 0.769 * *(p + 1) + 0.189 * *(p + 2), 255.0);         // red
+        *(pg + 1) = (uint8_t)fmin(0.349 * *p + 0.686 * *(p + 1) + 0.168 * *(p + 2), 255.0);         // green
+        *(pg + 2) = (uint8_t)fmin(0.272 * *p + 0.534 * *(p + 1) + 0.131 * *(p + 2), 255.0);         // blue        
+    }
+    return 0;
+}
+
 int main(void) {
+    const char *relativePath = "../img/";
+    char *name = "1";
     int width, height, channels;
+
+
+    char fullPath[80]; 
+    char fullName[40]; 
+    snprintf(fullName, sizeof(fullName), "%s%s", name , ".jpg");
+    snprintf(fullPath, sizeof(fullPath), "%s%s", relativePath, fullName);
     //unsigned char *img = stbi_load("sky.jpg", &width, &height, &channels, 0);
-    unsigned char *img = stbi_load("../img/1.jpg", &width, &height, &channels, 0);
+    unsigned char *img = stbi_load(fullPath, &width, &height, &channels, 0);
     if(img == NULL) {
         printf("Error in loading the image\n");
         exit(1);
@@ -20,8 +47,7 @@ int main(void) {
 
     // Convert the input image to gray
     size_t img_size = width * height * channels;
-    int gray_channels = channels == 4 ? 2 : 1;
-    size_t gray_img_size = width * height * gray_channels;
+    size_t gray_img_size = width * height;
 
     unsigned char *gray_img = malloc(gray_img_size);
     if(gray_img == NULL) {
@@ -29,15 +55,12 @@ int main(void) {
         exit(1);
     }
 
-    for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
-        *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
-        if(channels == 4) {
-            *(pg + 1) = *(p + 3);
-        }
-    }
+    gray(img, gray_img, img_size);
 
-    stbi_write_jpg("sky_gray.jpg", width, height, gray_channels, gray_img, 100);
-    //stbi_write_jpg("Shapes_gray.jpg", width, height, gray_channels, gray_img, width * gray_channels);
+    char GrayName[84]; 
+    snprintf(GrayName, sizeof(GrayName), "%s%s%s", relativePath, name, "_gray.jpg");
+    stbi_write_jpg(GrayName, width, height, 1, gray_img, 100);
+
 
     // Convert the input image to sepia
     unsigned char *sepia_img = malloc(img_size);
@@ -46,18 +69,11 @@ int main(void) {
         exit(1);
     }
 
-    // Sepia filter coefficients from https://stackoverflow.com/questions/1061093/how-is-a-sepia-tone-created
-    for(unsigned char *p = img, *pg = sepia_img; p != img + img_size; p += channels, pg += channels) {
-        *pg       = (uint8_t)fmin(0.393 * *p + 0.769 * *(p + 1) + 0.189 * *(p + 2), 255.0);         // red
-        *(pg + 1) = (uint8_t)fmin(0.349 * *p + 0.686 * *(p + 1) + 0.168 * *(p + 2), 255.0);         // green
-        *(pg + 2) = (uint8_t)fmin(0.272 * *p + 0.534 * *(p + 1) + 0.131 * *(p + 2), 255.0);         // blue        
-        if(channels == 4) {
-            *(pg + 3) = *(p + 3);
-        }
-    }
-
-    //stbi_write_jpg("sky_sepia.jpg", width, height, channels, sepia_img, 100);
-    stbi_write_png("Shapes_sepia.png", width, height, channels, sepia_img, width * channels);
+    sepia(img, sepia_img, img_size);
+    
+    char SepiaName[84]; 
+    snprintf(SepiaName, sizeof(SepiaName), "%s%s%s", relativePath, name, "_sepia.jpg");
+    stbi_write_png(SepiaName, width, height, channels, sepia_img, width * channels);
 
     stbi_image_free(img);
     free(gray_img);
