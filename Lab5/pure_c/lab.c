92be8ac016533da58b82e29e1f948150dd0f5bf9
EverydayBruh <roliki2222222@gmail.com>
2023-08-10 03:49:08 +0300

    Delete Trash

--- Lab5/pure_c/lab.c
+++ Lab5/pure_c/lab.c
@@ -17,14 +17,6 @@ int matrix [5][5] =
  1, 4, 6 , 4 , 1
 };
 
-int gray(unsigned char *img, unsigned char *gray_img, size_t img_size){
-
-    for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += 3, pg += 1) {
-        *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
-    }
-    return 0;
-}
-
 void blurpixel(unsigned char *img_pixel, unsigned char *blur_pixel, int width)
 {
     float sum = 0;
@@ -59,14 +51,6 @@ int blur(unsigned char *img, unsigned char *blur_img, int width, int hight){
     return 0;
 }
 
-int sepia(unsigned char *img, unsigned char *sepia_img, size_t img_size){
-    for(unsigned char *p = img, *pg = sepia_img; p != img + img_size; p += 3, pg += 3) {
-        *pg       = (uint8_t)fmin(0.393 * *p + 0.769 * *(p + 1) + 0.189 * *(p + 2), 255.0);         // red
-        *(pg + 1) = (uint8_t)fmin(0.349 * *p + 0.686 * *(p + 1) + 0.168 * *(p + 2), 255.0);         // green
-        *(pg + 2) = (uint8_t)fmin(0.272 * *p + 0.534 * *(p + 1) + 0.131 * *(p + 2), 255.0);         // blue        
-    }
-    return 0;
-}
 
 
 int main(void) {
@@ -86,36 +70,7 @@ int main(void) {
         exit(1);
     }
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
-
-    // Convert the input image to gray
     size_t img_size = width * height * channels;
-    size_t gray_img_size = width * height;
-
-    unsigned char *gray_img = malloc(gray_img_size);
-    if(gray_img == NULL) {
-        printf("Unable to allocate memory for the gray image.\n");
-        exit(1);
-    }
-
-    gray(img, gray_img, img_size);
-
-    char GrayName[84]; 
-    snprintf(GrayName, sizeof(GrayName), "%s%s%s", relativePath, name, "_gray.jpg");
-    stbi_write_jpg(GrayName, width, height, 1, gray_img, 100);
-
-
-    // Convert the input image to sepia
-    unsigned char *sepia_img = malloc(img_size);
-    if(sepia_img == NULL) {
-        printf("Unable to allocate memory for the sepia image.\n");
-        exit(1);
-    }
-
-    sepia(img, sepia_img, img_size);
-    
-    char SepiaName[84]; 
-    snprintf(SepiaName, sizeof(SepiaName), "%s%s%s", relativePath, name, "_sepia.jpg");
-    stbi_write_jpg(SepiaName, width, height, 3, sepia_img, 100);
 
 
     // Blur image
@@ -132,6 +87,5 @@ int main(void) {
     stbi_write_jpg(blurName, width, height, 3, blur_img, 100);
 
     stbi_image_free(img);
-    free(gray_img);
-    free(sepia_img);
+    free(blur_img);
 }
